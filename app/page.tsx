5480c1c61d6efeeb12cfb0d6d1c466e44977f5c7
Γιαννης Ζουμπουλακης <gianniszouboulakis@gmail.com>
2025-01-12 20:21:02 +0100

    f

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center bg-white min-h-screen sm:p-20 font-[family-name:var(--font-geist-sans)]">
